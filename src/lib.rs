5c25615af0e4b5e482d77ef95eb4c4294e0e92b9
Iosmanthus Teng <myosmanthustree@gmail.com>
2019-10-27 10:07:49 +0800

    rewrite the code
    Signed-off-by: Iosmanthus Teng <myosmanthustree@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,23 @@
+use std::io::{Error, ErrorKind, Result};
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use std::process::Command;
 
-pub fn get() -> Option<IpAddr> {
-    let output = match Command::new("hostname").args(&["-I"]).output() {
-        Ok(ok) => ok,
-        Err(_) => {
-            return None;
-        }
-    };
+pub fn get() -> Result<IpAddr> {
+    let output = Command::new("hostname").args(&["-i"]).output()?.stdout;
+    let source = String::from_utf8(output)
+        .map_err(|e| Error::new(ErrorKind::InvalidData, format!("{:?}", e)))?;
 
-    let stdout = match String::from_utf8(output.stdout) {
-        Ok(ok) => ok,
-        Err(_) => {
-            return None;
-        }
+    let ips = source.trim().split(" ").collect::<Vec<_>>();
+    let first = ips
+        .first()
+        .ok_or(Error::new(ErrorKind::NotFound, "Ip not found"))?;
+    return if let Ok(addr) = first.parse::<Ipv4Addr>() {
+        Ok(IpAddr::V4(addr))
+    } else if let Ok(addr) = first.parse::<Ipv6Addr>() {
+        Ok(IpAddr::V6(addr))
+    } else {
+        Err(Error::new(ErrorKind::NotFound, "Ip not found"))
     };
-
-    let ips: Vec<&str> = stdout.trim().split(" ").collect::<Vec<&str>>();
-    let first = ips.first();
-    match first {
-        Some(first) => {
-            if !first.is_empty() {
-                if let Ok(addr) = first.parse::<Ipv4Addr>() {
-                    return Some(IpAddr::V4(addr));
-                } else if let Ok(addr) = first.parse::<Ipv6Addr>() {
-                    return Some(IpAddr::V6(addr));
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        }
-        None => None,
-    }
 }
 
 #[cfg(test)]
@@ -41,8 +25,6 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        let res = get();
-        println!("res: {:?}", res);
-        assert!(res.is_some());
+        assert!(get().is_ok());
     }
 }
